1d8cec60273a70d4e88f0a0207155a644e3c6619
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-17 02:54:42 +0000

    Jul 17, 2026, 8:24 AM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -178,7 +178,8 @@ export const PracticeVideo = async (req, res) => {
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 6;
         const skip = (page - 1) * limit;
-        const search= (req.query.search) || ""
+        const search= (req.query.search) || "";
+        const sortOption= (req.query.search) || "latest";
         if (page < 1 || limit < 1 || limit > 12) {
             throw new apiError(401, "Invalid Query Parameters");
         }
