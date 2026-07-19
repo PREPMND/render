bc939ba34d5d41d55db01999d2f8fd9ac83d09aa
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-19 15:33:14 +0530

    modified user.controller.js in controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -121,7 +121,7 @@ const loginUser = asyncHandler(async (req, res, next) => {
     const options = {
         httpOnly: true,
         secure: isProd,
-        sameSite: isProd ? "none" : "lax",
+        sameSite: true,
         maxAge: 7 * 24 * 60 * 60 * 1000
     };
 
