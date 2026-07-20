44533d1d765d6e090091de8214945ead64e2612c
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-20 02:32:46 +0000

    Jul 20, 2026, 8:02 AM

--- src/controllers/socket.controller.js
+++ src/controllers/socket.controller.js
@@ -90,7 +90,7 @@ export const getConversations = asyncHandler(async (req, res) => {
                     conversationId: "$lastMessage.conversationId",
                     lastMessage: "$lastMessage.text",
                     createdAt: "$lastMessage.createdAt",
-                    lastSeen:"$lastMessage.lastSeen",
+                    lastSeen:"$lastMessage.LastSeen",
                     otherUser: {
                         _id: "$otherUser._id",
                         username: "$otherUser.username",
