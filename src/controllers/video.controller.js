d462da132a88cd14ac255574749265900c49e4b7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-21 14:52:42 +0000

    Jul 21, 2026, 8:22 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -486,7 +486,7 @@ const Solve = (page, limit, sort, search) => {
     const skip = (page - 1) * limit;
     const words = search.trim().split(/\s+/);
     let owner="";
-    const ownerToken=
+    const ownerToken=words.find(word=>)
     let owner = "";
     const words = search.trim().split(/\s+/);
     const ownerToken = words.find(word => word.startsWith("o/"));
