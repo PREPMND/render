d81c3f8f807c95011d064eb03d574e7539e600e2
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-21 15:02:53 +0000

    Jul 21, 2026, 8:32 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -558,8 +558,8 @@ export const BackendScale = (req, res) => {
     }
     const search = (req.query.search) || "";
 
-    const [video,] = Solve(page, limit, sort, search);
+    const [video,totalVideos] = Solve(page, limit, sort, search);
     return res.status(200).json(
-        new apiResponse(200, { result }, "the operation is successfull")
+        new apiResponse(200, { video}, "the operation is successfull")
     );
 }
