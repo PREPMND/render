d6c3945cf2d89b4b308827cd5fa41bce94696edc
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-22 16:17:02 +0000

    Jul 22, 2026, 9:47 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -648,5 +648,5 @@ export const buildVideoQuery = async (req, res) => {
     };
 }
 export const getVideosService=async(req,res)=>{
-    const {pageNumber,limitNumber,}
+    const {page:pageNumber,limitNumber,}
 }
