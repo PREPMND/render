f73b4e4ef36ff7b480ecc687e4359a45e47a2976
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-22 16:05:08 +0000

    Jul 22, 2026, 9:35 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -589,7 +589,7 @@ export const buildVideoQuery =async (req,res) => {
                 }
             })
             ownerIds=users.map(user=> user._id);
-        }//assuming like the base datao f
+        }//assuming like the base datao of video documents have only 
         if(search.trim()){
             filter.$or=[{
                 title:{
