78a244ab88c0b8bb4d9df69ef6354e71c091cc12
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-22 16:00:53 +0000

    Jul 22, 2026, 9:30 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -605,7 +605,7 @@ export const buildVideoQuery =async (req,res) => {
         }
         if(ownerIds.length>0){
             filter._id={
-                $in:ownerIds
+                $in:ownerIds,
             }
         }
 
