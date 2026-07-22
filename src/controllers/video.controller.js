1487fb779f76df16de85d60e6bc594bc2b06f6b4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-22 16:17:58 +0000

    Jul 22, 2026, 9:47 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -564,7 +564,7 @@ export const BackendScale = async (req, res) => {
         new apiResponse(200, { videos, totalVideos }, "the operation is successfull")
     );
 }
-export const buildVideoQuery = async (req, res) => {
+export const buildVideoQuery = async (req) => {
     const { page = 1,
         limit = 6,
         search = "",
