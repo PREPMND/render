23333a713d8016ddbaf4c5d6df63ad1cb4726012
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-22 16:10:04 +0000

    Jul 22, 2026, 9:40 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -633,6 +633,7 @@ export const buildVideoQuery = async (req, res) => {
     }
     if(createdAfter || createdBefore){
         filter.createdAt={};
+        if(createdAfter) filter.createdAt.$gte
     }
 
 }
