61dc1703ef504ab5ba7b2e12788a2b6c4e386430
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-22 16:05:28 +0000

    Jul 22, 2026, 9:35 PM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -589,7 +589,7 @@ export const buildVideoQuery =async (req,res) => {
                 }
             })
             ownerIds=users.map(user=> user._id);
-        }//assuming like the base datao of video documents have only ids of owner and ntg else 
+        }//assuming like the base datao of video documents have only ids of owner and ntg else. hence one did use ids. 
         if(search.trim()){
             filter.$or=[{
                 title:{
