310ac18b7220ccb91429a778da8ba111077c4cb4
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-23 14:37:22 +0000

    Jul 23, 2026, 8:07 PM

--- src/controllers/socket.controller.js
+++ src/controllers/socket.controller.js
@@ -194,7 +194,7 @@ export const markMessagesAsSeen = async (req, res) => {
         await Message.updateMany(
             {
                 conversationId,
-                otherUs: req.receiver,
+                receiver: req.receiver,
                 status: "sent",
             },
             {
