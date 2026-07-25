3896dfbe3cd0da1d7c19038cab40224a7c5e266b
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-25 12:23:21 +0000

    Jul 25, 2026, 5:53 PM

--- src/middlewares/validatePagination.middleware.js
+++ src/middlewares/validatePagination.middleware.js
@@ -1,16 +1,15 @@
 import { apiError } from "../utils/apiError.js";
 export const validatePagination=async(req,res)=>{
     try {
-        const page=(req.query.page)||1;
+        const page=Number(req.query.page)||1;
         if(page<1 || isNaN(Number(page))){
             throw new apiError(401,"Page parameters are wrong.");
         }
-        const limit=(req.query.limit) || 6;
+        const limit=Number(req.query.limit) || 6;
         if(limit>10 || limit<1 || isNaN(Number(limit))){
             throw new apiError(401,"Limit should'nt exceed 10 and should be a number");
         }
         page=Number(page);
-        limit=Number(limit);
         req.validatedQuery={
             ...req.validatedQuery,
             page,
