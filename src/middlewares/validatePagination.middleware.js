9983e7fd56c8b2302b62161c2bc9bb14992913d1
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-25 12:14:54 +0000

    Jul 25, 2026, 5:44 PM

--- src/middlewares/validatePagination.middleware.js
+++ src/middlewares/validatePagination.middleware.js
@@ -1,8 +1,8 @@
-import apiError
+import { apiError } from "../utils/apiError.js";
 export const validatePagination=async(req,res)=>{
     const page=(req.query.page)|| 1;
     if(page<1 || !isNaN(Number(page))){
-        throw new 
+        throw new apiError
     }
     const limit=(req.query.limit) ||6;
 
