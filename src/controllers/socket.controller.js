493ffc7ac36b47feb8924d11b4b3b2a4f536bd39
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-25 01:36:53 +0000

    Jul 25, 2026, 7:06 AM

--- src/controllers/socket.controller.js
+++ src/controllers/socket.controller.js
@@ -7,10 +7,7 @@ import redis from "../redis/redis.js"
 export const getConversationId = (user1, user2) => {
     return [user1.toString(), user2.toString()].sort().join("_");
 };
-const emitToConversation = (req, conversationId, event, payload) => {
-    const io = req.app.get("io");
-    io.to(conversationId).emit(event, payload);
-};
+
 export const sendMessage = asyncHandler(async (req, res) => {
     const sender = req.user?._id;
     const { receiver, text, messageType = "text" } = req.body;
