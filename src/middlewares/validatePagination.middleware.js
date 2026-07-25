42e1fde65f9ac4fe13dead919003ad49fbed2d2a
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-25 12:14:19 +0000

    Jul 25, 2026, 5:44 PM

--- src/middlewares/validatePagination.middleware.js
+++ src/middlewares/validatePagination.middleware.js
@@ -1,6 +1,8 @@
 export const validatePagination=async(req,res)=>{
     const page=(req.query.page)|| 1;
-    if(page<1 || !isNaN(Number(page)))
+    if(page<1 || !isNaN(Number(page))){
+        throw new 
+    }
     const limit=(req.query.limit) ||6;
 
 }
