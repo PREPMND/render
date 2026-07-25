68d7f9a4bd8d68e415ab645b8624584a243a2b68
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-25 12:19:56 +0000

    Jul 25, 2026, 5:49 PM

--- src/middlewares/validatePagination.middleware.js
+++ src/middlewares/validatePagination.middleware.js
@@ -18,6 +18,6 @@ export const validatePagination=async(req,res)=>{
         }
         next();
     } catch (error) {
-        throw new apiError(401,"")
+        throw new apiError(401,`The request resp`)
     }
 }
