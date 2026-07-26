b9a690e98bf1cc07c3b4a2e9a1abb5bc6c8664f7
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-07-26 03:03:05 +0000

    Jul 26, 2026, 8:33 AM

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -728,7 +728,7 @@ export const queryBuilder = async (query, options) => {
         sortOption,
         page:query.page,
         limit:query.limit,
-        skip: (query.page - 1) * query.limit
+        skip: (query.page - 1)*query.limit
 
     }
 
