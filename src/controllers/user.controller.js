67b0213086b98a059414aea8fbdf458ad9332a5f
Srijan <219679461+PREPMND@users.noreply.github.com>
2026-09-06 05:12:58 +0000

    Sep 6, 2026, 10:42 AM

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -79,7 +79,7 @@ const registerUser = asyncHandler(async (req, res, next) => {
         username: username.toLowerCase()
     })
     //if user empty?
-    const createdUser = await User.findById(user._id).select(
+    const createdUser =await User.findById(user._id).select(
         "-password -refreshToken"
     )
     if (!createdUser) {
